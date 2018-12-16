22e51a24e8ab3f707977b92551226dc26485374b
Stefan Ristanović <st.keky@gmail.com>
2018-12-16 01:47:00 +0100

    Refactor whole codebase

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # es-serbian-plugin
 
-Elasticsearch plugin containing Serbian language analyzer.
+Elasticsearch plugin providing support for Serbian language.
+
+## Description
+
+This plugin is used for preparing and analyzing text for indexing. It origin from [this repo][www:uddrepo] which was
+written based on following dependencies:
+- Apache Lucene **v4.9**
+- Elasticsearch **v1.3.2**
+
+Since these versions are highly outdated and unusable with newer versions of *Elasticsearch*, 
+plugin codebase was rewritten and adapted to use these dependencies:
+- Apache Lucene **v7.2.1**
+- Elasticsearch **v6.2.0**
+
+Unused, deprecated and redundant code is removed. Several parts are optimized, mainly with the use of **Apache Commons Lang** library.
+
+## Processing
+
+**SerbianAnalyzer** uses following components during the analysis:
+1. `StandardTokenizer`
+2. `LowerCaseFilter`
+3. `LatCyrFilter` (converts from cyrillic to latin letters)
+4. `StopFilter`
+5. `SnowballFilter` with SerbianStemmer (stems tokens, taking only root of the word)
+6. `RemoveAccentsFilter` (remove accents from the letters)
+
+## Build
+
+To build plugin run the following command:
+```
+mvn clean install
+```
+
+## Install
+
+To install build plugin to Elasticsearch run the following command:
+```
+elasticsearch-plugin install file:///path/to/plugin.zip
+```
+
+Analyzer provided by this plugin can afterwards be used in Elasticsearch environment by invocation via
+**serbian-analyzer** name.
+
+## Additional notes
+
+Original stemmer written in [Snowball][www:snowball] string processing language is provided in the
+ `resources/serbian-stemmber.sbl`. But, with the latest *Snowball compiler* is unable to compile it without errors.
+ Because of that already generated `SerbianStemmer` (generated earlier) was just slightly adapted so
+ it can be used with these newer version of *Lucene* dependencies.
+ 
+
+[www:uddrepo]: https://github.com/chenejac/udd06
+[www:snowball]: http://snowballstem.org/
--- src/main/java/org/elasticsearch/keky/io/SerbianAnalyzer.java
+++ src/main/java/org/elasticsearch/keky/io/SerbianAnalyzer.java
@@ -2,59 +2,49 @@ package org.elasticsearch.keky.io;
 
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.util.Version;
 
 
 public class SerbianAnalyzer extends Analyzer {
-
-    private CharArraySet serbianStopSet;
 
     private static final String[] SERBIAN_STOP_WORDS = {
             "biti", "ne",
             "jesam", "sam", "jesi", "si", "je", "jesmo", "smo", "jeste", "ste", "jesu", "su",
             "nijesam", "nisam", "nijesi", "nisi", "nije", "nijesmo", "nismo", "nijeste", "niste", "nijesu", "nisu",
-            "budem", "budeš", "bude", "budemo", "budete", "budu",
-            "budes",
-            "bih", "bi", "bismo", "biste", "biše",
-            "bise",
+            "budem", "budeš", "bude", "budemo", "budete", "budu", "budes",
+            "bih", "bi", "bismo", "biste", "biše", "bise",
             "bio", "bili", "budimo", "budite", "bila", "bilo", "bile",
             "ću", "ćeš", "će", "ćemo", "ćete",
             "neću", "nećeš", "neće", "nećemo", "nećete",
             "cu", "ces", "ce", "cemo", "cete",
             "necu", "neces", "nece", "necemo", "necete",
             "mogu", "možeš", "može", "možemo", "možete",
-            "mozes", "moze", "mozemo", "mozete"};
+            "mozes", "moze", "mozemo", "mozete"
+    };
 
-    public SerbianAnalyzer(Version version) {
-        super();
-        serbianStopSet = StopFilter.makeStopSet(SERBIAN_STOP_WORDS);
-    }
 
-    public SerbianAnalyzer() {
+    SerbianAnalyzer() {
         super();
-        serbianStopSet = StopFilter.makeStopSet(SERBIAN_STOP_WORDS);
     }
 
     @Override
-    protected TokenStreamComponents createComponents(String s) {
-        Tokenizer source = new StandardTokenizer();
+    protected TokenStreamComponents createComponents(String fieldName) {
+        final Tokenizer source = new StandardTokenizer();
 
         TokenStream result = new LowerCaseFilter(source);
 
         result = new LatCyrFilter(result);
 
-        result = new StopFilter(result, serbianStopSet);
+        result = new StopFilter(result, StopFilter.makeStopSet(SERBIAN_STOP_WORDS));
 
         result = new SnowballFilter(result, new SerbianStemmer());
 
-        result = new RemoveAccentsFilter(result, true);
+        result = new RemoveAccentsFilter(result);
 
         return new TokenStreamComponents(source, result);
     }
